62964cfdcbf4582451b188aa85822caf2db2525d
Елизавета Горшенина <liza_summer@mail.ru>
2023-04-26 01:12:17 +0300

    lesson_8 Receipt and sending of messages implemented via threads on the client side

--- client_7.py
+++ client_7.py
@@ -1,31 +1,42 @@
 from socket import *
+from threading import Thread
+import time
 
 
 ADDRESS = ('localhost', 10000)
 
 
 def set_read(sock):
-    data = sock.recv(1024)
-    return data.decode('utf-8')
+    while True:
+        data = sock.recv(1024)
+        print('Ответ:', data.decode('utf-8'))
 
 
-def set_write(sock, msg):
-    sock.send(msg.encode('utf-8'))
+def set_write(sock):
+    while True:
+        msg = input('Ваше сообщение: ')
+        if msg == 'exit':
+            break
+        else:
+            sock.send(msg.encode('utf-8'))
 
 
 def client_mainloop():
     with socket(AF_INET, SOCK_STREAM) as sock:
         sock.connect(ADDRESS)
+        msg_receipt = Thread(target=set_read, args=(sock,))
+        msg_receipt.daemon = True
+        msg_receipt.start()
+
+        msg_sending = Thread(target=set_write, args=(sock,))
+        msg_sending.daemon = True
+        msg_sending.start()
+
         while True:
-            sock_option = input('Принять/отправить сообщение: ')
-            if sock_option == 'exit':
-                break
-            if sock_option == 'r':
-                data = set_read(sock)
-                print('Ответ:', data)
-            if sock_option == 'w':
-                msg = input('Ваше сообщение: ')
-                set_write(sock, msg)    
+            time.sleep(1)
+            if msg_receipt.is_alive() and msg_sending.is_alive():
+                continue
+            break
 
 
 if __name__ == '__main__':
